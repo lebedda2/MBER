d7099124d6c0b08ee471850212ca097d401923cb
lebedda2 <lebedda2@umbc.edu>
2017-04-07 08:24:37 -0400

    Initial Commit of ROS framework

---
+++ mber_bot/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/mber/MBER/mber_bot/devel:$CMAKE_PREFIX_PATH"
+export PWD="/home/mber/MBER/mber_bot/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/mber/MBER/mber_bot/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/mber/MBER/mber_bot/src:$ROS_PACKAGE_PATH"
---
+++ mber_console/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/mber/MBER/mber_console/src
+# Build directory: /home/mber/MBER/mber_console/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(gtest)
