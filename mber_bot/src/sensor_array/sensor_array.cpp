84004d3ef17e070c5e4e7c690f0fac69034308bf
MBER <mber@mber_bot.localdomain>
2017-05-01 23:29:16 +0000

    Temperature Sensor Fully Integrated

--- mber_bot/src/sensor_array/sensor_array.cpp
+++ mber_bot/src/sensor_array/sensor_array.cpp
@@ -1,5 +1,6 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
+#include "std_msgs/Float32.h"
 #include <errno.h>
 #include <string.h>
 #include <stdio.h>
@@ -19,26 +20,30 @@
 #define I2C_DEV_NAME   "/dev/i2c-2"
 #define I2C_FNAME_SIZE 40
 
-int get_TEMP_data(int i2c_file)
+float get_TEMP_data(int i2c_file)
 {
-	int16_t raw;
-	float   data;
-	char    channel;
-	int     i;
+	uint16_t raw;
+	char     tmp[4];
+	float    data;
+	char     channel;
+	int      i;
 
 	if (ioctl(i2c_file, I2C_SLAVE, TEMP_I2C_POS) < 0) {
 		printf("Failed to acquire bus access and/or talk to temperature slave.\n");
 		exit(1);
 	}
 
-	if (read(i2c_file, &raw, 2) != 2) {
+	if (read(i2c_file, &tmp, 4) != 4) {
 		printf("Failed to read from the i2c bus.\n\n");
 	} else {
+		raw = (uint16_t)((tmp[0] << 8) + tmp[1]);
+		printf("Step 1: byte = %x\n", raw);
 		raw >>= 2;
-		raw *= 0.03125;
+		data = (float)raw;
+		data *= 0.03125;
 	}
 
-	return (int)raw;
+	return data;
 }
 
 int get_CO2_data(void)
@@ -55,10 +60,15 @@ int main(int argc, char **argv)
 {
 	/** ROS Init Stuff **/
 	ros::init(argc, argv, "mber_bot_sensors");
-	ros::NodeHandle n;
-	ros::Publisher sensor_publish_CO2 = n.advertise<std_msgs::String>("mber_sensors_CO2", 1000);
-	ros::Publisher sensor_publish_radiation = n.advertise<std_msgs::String>("mber_sensors_radiation", 1000);
-	ros::Publisher sensor_publish_temperature = n.advertise<std_msgs::String>("mber_sensors_temperature", 1000);
+	ros::NodeHandle n_CO2;
+	ros::NodeHandle n_temp;
+	ros::NodeHandle n_geiger;
+	ros::Publisher sensor_publish_CO2 =
+		n_CO2.advertise<std_msgs::String>("mber_sensors_CO2", 1000);
+	ros::Publisher sensor_publish_radiation =
+		n_geiger.advertise<std_msgs::String>("mber_sensors_radiation", 1000);
+	ros::Publisher sensor_publish_temperature =
+		n_temp.advertise<std_msgs::Float32>("mber_sensors_temperature", 1000);
 	ros::Rate loop_rate(10);
 
 	/** I2C Init Stuff **/
@@ -72,13 +82,12 @@ int main(int argc, char **argv)
 
 	int count = 0;
 	while (ros::ok()) {
-		std_msgs::String msg;
+		std_msgs::Float32 msg;
+		float msg_data = get_TEMP_data(i2c_file);
 
-		std::stringstream ss;
-		ss << get_TEMP_data(i2c_file);
-		msg.data = ss.str();
+		msg.data = msg_data;
 
-		ROS_INFO("Temperature: %s", msg.data.c_str());
+		ROS_INFO("Temperature: %f", msg_data);
 
 		sensor_publish_temperature.publish(msg);
 
